304e6562c3dad516c5a8a6391a8649ffec65c16a
SM Farhad Ali <farhad.ali@iais.fraunhofer.de>
2022-02-19 15:15:04 +0100

    Vuetify added

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,27 +1,65 @@
 <template>
-  <div id="app">
-    <Playground msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer overlay-color="white" v-model="drawer" width="324" app>
+      <template v-slot:prepend>
+        <v-list-item>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg" />
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title>Jane Smith</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item v-for="item in items" :key="item.title">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+      <Playground />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Playground from './components/Playground.vue'
+import HelloWorld from "./components/HelloWorld";
+import Playground from "./components/Playground";
 
 export default {
-  name: 'App',
-  components: {
-    Playground
-  }
-}
-</script>
+  name: "App",
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    Playground
+  },
+
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Home", icon: "mdi-home-city" },
+      { title: "My Account", icon: "mdi-account" },
+      { title: "Users", icon: "mdi-account-group-outline" },
+    ],
+  }),
+};
+</script>
