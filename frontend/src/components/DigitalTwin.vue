6ddc01fe8035a3e6189081319e7dd6aa2f25dbef
SM Farhad Ali <farhad.ali@iais.fraunhofer.de>
2022-03-18 01:56:14 +0100

    Swtich board added

--- frontend/src/components/DigitalTwin.vue
+++ frontend/src/components/DigitalTwin.vue
@@ -3,10 +3,46 @@
     <v-row>
       <v-col md="4"> </v-col>
       <v-col md="4">
-        <v-card class="pa2">
-          <v-card-title> This card is for switch board </v-card-title>
+        <v-card>
+          <v-card-title> Switch board </v-card-title>
           <v-card-text>
-            <v-spacer></v-spacer>
+            <v-row>
+              <v-col md="6">
+                <v-list flat>
+                  <v-list-item
+                    v-for="(item, i) in items.slice(
+                      0,
+                      Math.ceil(items.length / 2)
+                    )"
+                    :key="i"
+                  >
+                    <v-list-item-icon>
+                      <v-switch inset></v-switch>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-col>
+              <v-col md="6">
+                <v-list flat>
+                  <v-list-item
+                    v-for="(item, i) in items.slice(
+                      -Math.ceil(items.length / 2)
+                    )"
+                    :key="i"
+                  >
+                    <v-list-item-icon>
+                      <v-switch inset></v-switch>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title v-text="item.text"></v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list>
+              </v-col>
+            </v-row>
           </v-card-text>
         </v-card>
       </v-col>
@@ -27,7 +63,17 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data: () => ({
+    items: [],
+  }),
+
+  created() {
+    for (let i = 1; i <= 8; i++) {
+      this.items.push({ text: "Port " + i });
+    }
+  },
+};
 </script>
 
 <style scoped></style>
